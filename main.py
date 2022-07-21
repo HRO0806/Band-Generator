8c83c3a4d4b8e257830abea1e261e1eb5dc2e893
Henry <henryolson@icloud.com>
2022-07-21 15:30:25 -0500

    First Python project

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+def band_name():
+  print("Welcome to Band name generator!")
+  city = input("What city did you grow up in?\n")
+  pet_name = input("What is your pets name?\n")
+  print("Your new band name is " + city + " " + pet_name)
+
+band_name()   
